b7b167cf2e79aacb5e19498f16b773e64ab8e74d
Sergiy Petruniv <petruniv.s.i@gmail.com>
2019-06-16 21:38:42 +0300

    add part of slide-show

--- script/script.js
+++ script/script.js
@@ -8,20 +8,78 @@
   //event for hidden navbar
   burger.addEventListener('click', function () {
 
-      if (links.style.display === "block") {
-          links.classList.toggle('d-flex-ul');
-          burger.classList.toggle("change");
-      } else {
-          burger.classList.toggle("change");
-          links.classList.toggle('d-flex-ul');
-      }
+    if (links.style.display === "block") {
+      links.classList.toggle('d-flex-ul');
+      burger.classList.toggle("change");
+    } else {
+      burger.classList.toggle("change");
+      links.classList.toggle('d-flex-ul');
+    }
   });
 
 
   // events for login popup
-  closePopup.addEventListener('click', (e)=>{
+  closePopup.addEventListener('click', (e) => {
     loginPopBox.style.display = "none";
   })
-  logButton.addEventListener('click', ()=>{
+  logButton.addEventListener('click', () => {
     loginPopBox.style.display = "block";
   })
+
+
+  /* gallery slideshow */
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function nextSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("gallery-slides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+  }
+
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("gallery-slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+} 
+
+///////////////////
+
+
+(function(){
+
+
+
+})());
