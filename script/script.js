eba34dde06eaa2884083ca16974a241afcd948e5
Sergiy Petruniv <petruniv.s.i@gmail.com>
2019-06-17 14:19:43 +0300

    add full gallery

--- script/script.js
+++ script/script.js
@@ -24,62 +24,49 @@
   })
   logButton.addEventListener('click', () => {
     loginPopBox.style.display = "block";
-  })
+  });
 
 
   /* gallery slideshow */
+  (function () {
+    var slides = document.querySelectorAll('.gallery-slides');
+    let prev = document.querySelector('.prev');
+    let next = document.querySelector('.next');
+    var container = document.querySelector('.gallery-container');
+    var currentSlide = 0;
+    var timer;
 
-  var slideIndex = 1;
-  showSlides(slideIndex);
+    startSlideshow();
+    timer = setInterval(startSlideshow, 3000);
 
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-
-  function nextSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("gallery-slides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-      slideIndex = 1
+    function startSlideshow() {
+      slides[currentSlide].className = 'gallery-slides ';
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].className = 'gallery-show fade';
     }
-    if (n < 1) {
-      slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-  }
 
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("gallery-slides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-} 
+    prev.addEventListener('click', function () {
+      slides[currentSlide].className = 'gallery-slides ';
+      if(currentSlide == 0) {
+        currentSlide = slides.length;
+      }
+      currentSlide = (currentSlide - 1) % slides.length;
+      slides[currentSlide].className = 'gallery-show fade';
 
-///////////////////
+    });
 
-
-(function(){
-
-
-
-})());
+    next.addEventListener('click', function () {
+      slides[currentSlide].className = 'gallery-slides ';
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].className = 'gallery-show fade';
+    });
+    
+    /* stop slideshow when mouseover*/
+    container.addEventListener('mouseover', function(){
+      clearInterval(timer)
+    });
+    container.addEventListener('mouseout', function(){
+      timer = setInterval(startSlideshow, 3000);
+    });
+  })();
